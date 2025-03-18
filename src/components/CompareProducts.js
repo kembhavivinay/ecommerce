5ecfb225d55e47780ffbc5b7c1c298988710f562
kembhavivinay <86705970+kembhavivinay@users.noreply.github.com>
2025-03-18 10:53:53 +0530

    few update added for compare products

--- src/components/CompareProducts.js
+++ src/components/CompareProducts.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import {Button,Table} from "antd"
+import React, {useState } from 'react';
+import {Button,Table,message} from "antd"
 
 const CompareProducts = () => {
     const [selectedProducts,setSelectedProducts] = useState(() =>{
@@ -11,8 +10,18 @@ const CompareProducts = () => {
     });
 
     const removeProduct = (id) =>{
-
+        setSelectedProducts(selectedProducts.filter((p) => p.id !== id));
     }
+
+    const addProduct = () =>{
+        if (selectedProducts.length >= 4) {
+            message.error("Compare only 4 products");
+            return;
+        }
+        const newProduct = {"id":2,"title":"Eyeshadow Palette with Mirror","price":100,"brand":"newBrand","rating":"6.6"}
+        setSelectedProducts([...selectedProducts,newProduct]);
+    }
+
     const columns = [
         {
           title: "Feature",
@@ -27,14 +36,13 @@ const CompareProducts = () => {
         })),
       ];
     
-        const features = ["Price", "Brand", "Rating", "Action"];
+        const features = ["Price", "Brand", "Category", "Action"];
         const data = features.map((feature, index) => {
         const row = { key: (index + 1).toString(), feature };
         selectedProducts.forEach((p,productIndex) => {
-            console.log(productIndex)
             if (feature === "Price") row[p.id] = p.price;
             else if (feature === "Brand") row[p.id] = p.brand;
-            else if (feature === "Rating") row[p.id] = p.rating;
+            else if (feature === "Category") row[p.id] = p.category;
             else if (feature === "Action" && productIndex !== 0) row[p.id] =  <Button danger onClick={() => removeProduct(p.id)}>Remove</Button>;
         });
 
@@ -44,6 +52,7 @@ const CompareProducts = () => {
         <div>
             <h2>Compare Products</h2>
             <Table columns={columns} dataSource={data} pagination={false} bordered />
+            <Button onClick={() => addProduct()}>Add More</Button>
         </div>
     );
 }
